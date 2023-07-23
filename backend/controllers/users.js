525d63aecf7f391974d32f919b0658dc50b3d5aa
Lexev <evtenkoaleksei@gmail.com>
2023-07-23 18:30:54 +0300

    jwt issue

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -18,7 +18,8 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          // sameSite: 'None',
+          sameSite: 'none',
+          secure: true,
         })
         .send({ message: 'Авторизация прошла успешно!' });
     })
