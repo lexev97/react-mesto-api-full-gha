3e5f61838c7286e9100b02c84adf430c4c4cc86a
Lexev <evtenkoaleksei@gmail.com>
2023-07-24 02:28:09 +0300

    first review corrections

--- backend/middlewares/limiter.js
+++ backend/middlewares/limiter.js
@@ -1,10 +1,8 @@
-const rateLimit = require('express-rate-limit');
+const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000,
-	max: 100,
-	standardHeaders: true,
-	legacyHeaders: false,
-})
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 module.exports = { limiter };
